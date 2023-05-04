4021b521d11799e3efadd268ce7a3c606ae9c972
sadatebne <sadatshafin75@gmail.com>
2023-05-05 02:56:32 +0600

    first commit server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+app.use(cors())
+
+const chefs=require('./data/chef.json')
+const recipes= require('./data/recipes.json')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+  })
+
+app.get('/chefs/:id', (req, res) => {
+    const id=parseInt(req.params.id)
+    const chefRecipe=recipes.filter(r=>parseInt(r.chefId)===id)
+    res.send(chefRecipe)
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
